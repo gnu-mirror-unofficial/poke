6b59deb3ec0c0ab0ca4426fb6ebcbd7fb05f6aed
John Darrington <john@darrington.wattle.id.au>
2020-05-02 10:39:15 +0200

    Doc: Correct recurring typo in documentation and add a check.
    Change "Lets" to "Let's" and add a syntax-check test so this
    problem won't keep comming back.

--- cfg.mk
+++ cfg.mk
@@ -6,3 +6,11 @@ export _gl_TS_headers = *.h
 local-checks-to-skip =                  \
    sc_tight_scope \
    sc_prohibit_gnu_make_extensions
+
+sc_jemarchism.z:
+	@prohibit='(^|[ ])[Ll]ets( |$$)'				\
+	in_vc_files='$(texinfo_suffix_re_)'				\
+	halt='found use of "lets" in Texinfo source'			\
+	  $(_sc_search_regexp)
+
+local-checks-available=sc_jemarchism
