3b3bef6d787513300c9e1fef2b171f9702824ebb
Jose E. Marchesi <jose.marchesi@oracle.com>
2020-08-26 08:43:19 +0200

    libpoke,poke: fix regression in pk-val.c
    2020-08-26  Jose E. Marchesi  <jemarch@gnu.org>
    
    	* libpoke/pk-val.c (pk_type_code): Fix regression.
    	* poke/pk-map.c (pk_map_load_parsed_map): Remove spurious trace.

--- libpoke/pk-val.c
+++ libpoke/pk-val.c
@@ -164,7 +164,7 @@ pk_type_code (pk_val val)
   switch (PVM_VAL_TYP_CODE (val))
     {
     case PVM_TYPE_INTEGRAL:
-      if ((pk_int_value (pk_integral_type_signed_p (pk_typeof (val)))))
+      if ((pk_int_value (pk_integral_type_signed_p (val))))
         return PK_INT;
       else
         return PK_UINT;
