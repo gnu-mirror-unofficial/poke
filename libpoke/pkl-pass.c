e63ba0f3168337957d203e31be7cd63b930a3705
Jose E. Marchesi <jose.marchesi@oracle.com>
2020-11-24 20:16:01 +0100

    pkl: fix bug in traversing `for' loop AST nodes
    2020-11-24  Jose E. Marchesi  <jemarch@gnu.org>
    
    	* libpoke/pkl-pass.c (pkl_do_pass_1): Traverse the head of for
    	loops before the condition.
    	* poke/pk-copy.pk (pk_help_str): Use a `for' loop.

--- libpoke/pkl-pass.c
+++ libpoke/pkl-pass.c
@@ -525,10 +525,10 @@ pkl_do_pass_1 (pkl_compiler compiler,
     case PKL_AST_LOOP_STMT:
       if (PKL_AST_LOOP_STMT_ITERATOR (node))
         PKL_PASS (PKL_AST_LOOP_STMT_ITERATOR (node));
-      if (PKL_AST_LOOP_STMT_CONDITION (node))
-        PKL_PASS_CHAIN (PKL_AST_LOOP_STMT_CONDITION (node));
       if (PKL_AST_LOOP_STMT_HEAD (node))
         PKL_PASS (PKL_AST_LOOP_STMT_HEAD (node));
+      if (PKL_AST_LOOP_STMT_CONDITION (node))
+        PKL_PASS_CHAIN (PKL_AST_LOOP_STMT_CONDITION (node));
       if (PKL_AST_LOOP_STMT_TAIL (node))
         PKL_PASS_CHAIN (PKL_AST_LOOP_STMT_TAIL (node));
       PKL_PASS (PKL_AST_LOOP_STMT_BODY (node));
