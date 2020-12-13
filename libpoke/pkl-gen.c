3a13b44454a8f691ee2a5bf8cbc33bbd127bfa4b
Jose E. Marchesi <jose.marchesi@oracle.com>
2020-12-13 22:07:39 +0100

    pkl,doc,testsuite: support for the 'strict attribute
    2020-12-13  Jose E. Marchesi  <jemarch@gnu.org>
    
    	* libpoke/pkl-attrs.def: Define PKL_AST_ATTR_STRICT.
    	* libpoke/pkl-typify.c (pkl_typify1_ps_attr): Typify attribute
    	'strict.
    	* libpoke/pkl-gen.c (pkl_gen_ps_op_attr): Generate code for the
    	attribute 'strict.
    	* testsuite/poke.map/strict-attr-1.pk: New test.
    	* testsuite/poke.map/strict-attr-2.pk: Likewise.
    	* testsuite/poke.map/strict-attr-3.pk: Likewise.
    	* testsuite/poke.map/strict-attr-4.pk: Likewise.
    	* testsuite/poke.map/strict-attr-5.pk: Likewise.
    	* testsuite/poke.map/strict-attr-6.pk: Likewise.
    	* testsuite/poke.map/strict-attr-7.pk: Likewise.
    	* doc/poke.texi (Array Attributes): Document the 'strict
    	attribute.
    	(Non-strict Mapping): Mention the attribute.

--- libpoke/pkl-gen.c
+++ libpoke/pkl-gen.c
@@ -3874,6 +3874,23 @@ PKL_PHASE_BEGIN_HANDLER (pkl_gen_ps_op_attr)
           break;
         }
       break;
+    case PKL_AST_ATTR_STRICT:
+      switch (PKL_AST_TYPE_CODE (operand_type))
+        {
+        case PKL_TYPE_ANY:
+        case PKL_TYPE_ARRAY:
+        case PKL_TYPE_STRUCT:
+          pkl_asm_insn (PKL_GEN_ASM, PKL_INSN_MGETS);
+          pkl_asm_insn (PKL_GEN_ASM, PKL_INSN_NIP);
+          break;
+        default:
+          /* Other types are considered strict.  */
+          pkl_asm_insn (PKL_GEN_ASM, PKL_INSN_DROP);
+          pkl_asm_insn (PKL_GEN_ASM, PKL_INSN_PUSH,
+                        pvm_make_int (1, 32));
+          break;
+        }
+      break;
     default:
       PKL_ICE (PKL_AST_LOC (exp),
                "unhandled attribute expression code #%d in code generator",
